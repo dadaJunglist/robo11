46c296fcaf8fd82b84cde97fa1fb0ec8d7da4f87
shreyakhetani <112071340+shreyakhetani@users.noreply.github.com>
2023-03-29 16:27:27 +0300

    Obstacle avodance

--- src/app/Motor.java
+++ src/app/Motor.java
@@ -4,16 +4,21 @@ import lejos.hardware.Button;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 
-public class Motor extends Thread{
+public class Motor extends Thread {
 
 	private DataExchange dataExchange;
 
 	private static EV3LargeRegulatedMotor leftWheel;
 	private static EV3LargeRegulatedMotor rightWheel;
 
+<<<<<<< Updated upstream
 	private float SPEED_MAX = 200;
 	private float SPEED= (SPEED_MAX*6.075f)/10;
 
+=======
+	private static final int SPEED_MAX = 200;
+	private static final float SPEED = (SPEED_MAX * 6.07f) / 10;
+>>>>>>> Stashed changes
 
 	public Motor(DataExchange dataExchange) {
 
@@ -21,18 +26,30 @@ public class Motor extends Thread{
 
 		leftWheel = new EV3LargeRegulatedMotor(MotorPort.A);
 		rightWheel = new EV3LargeRegulatedMotor(MotorPort.D);
+<<<<<<< Updated upstream
 		
+=======
+
+		leftWheel.setSpeed(SPEED_MAX);
+		rightWheel.setSpeed(SPEED_MAX);
+>>>>>>> Stashed changes
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+<<<<<<< Updated upstream
 		
 		leftWheel.setSpeed(SPEED_MAX);
 		rightWheel.setSpeed(SPEED_MAX);
+=======
 
-		while(!Button.ESCAPE.isDown()) {
+		while (!Button.ESCAPE.isDown()) {
+>>>>>>> Stashed changes
 
+			if (dataExchange.getObstaclesDetected() == false) {
+
+<<<<<<< Updated upstream
 			if(dataExchange.getObstaclesDetected() == true){
 				
 				
@@ -71,6 +88,8 @@ public class Motor extends Thread{
 				
 			}else {
 				
+=======
+>>>>>>> Stashed changes
 				try {
 					Thread.sleep(10);
 				} catch (InterruptedException e) {
@@ -78,31 +97,68 @@ public class Motor extends Thread{
 					e.printStackTrace();
 				}
 
+<<<<<<< Updated upstream
 				
 				if(dataExchange.getCommand() == 1) {
+=======
+				if (dataExchange.getCommand() == 1) {
+>>>>>>> Stashed changes
 
 					leftWheel.setSpeed(SPEED);
 					rightWheel.setSpeed(SPEED_MAX);
 					leftWheel.backward();
 					rightWheel.forward();
 
-				}else if (dataExchange.getCommand() == 2) {
+				} else if (dataExchange.getCommand() == 2) {
 
 					leftWheel.setSpeed(SPEED_MAX);
 					rightWheel.setSpeed(SPEED_MAX);
 					leftWheel.forward();
 					rightWheel.forward();
 
-				}else if (dataExchange.getCommand() == 3) {
+				} else if (dataExchange.getCommand() == 3) {
 
 					leftWheel.setSpeed(SPEED_MAX);
 					rightWheel.setSpeed(SPEED);
 					leftWheel.forward();
 					rightWheel.backward();
-
 				}
+
+				} else {
+
+//					leftWheel.stop();
+//					rightWheel.stop();
+
+					leftWheel.setSpeed(300);
+					rightWheel.setSpeed(128);
+
+					try {
+						Thread.sleep(1500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					leftWheel.setSpeed(170);
+					rightWheel.setSpeed(300);
+					
+					
+					try {
+						Thread.sleep(4250);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+					leftWheel.setSpeed(300);
+					rightWheel.setSpeed(128);
+					
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
 				
-				
+
 			}
 
 			// Moving the robot forward
@@ -114,7 +170,6 @@ public class Motor extends Thread{
 		// Stop the motors
 		leftWheel.stop(true);
 		rightWheel.stop(true);
-
 
 	}
 
