2517acc1b0e010d2f13267fa7dc483190ec1a826
marikadu <marika.duhhanina@student.hamk.fi>
2023-03-30 09:08:09 +0300

    fixed main celebration threads

--- src/app/MainCeleb.java
+++ src/app/MainCeleb.java
@@ -2,21 +2,17 @@ package app;
 
 public class MainCeleb {
 	static Disco disco = new Disco();
-	static Thread disco_run = new Thread(disco);
-	
-	
+
 	static Music music = new Music();
-	static Thread music_run = new Thread(music);
-	
+
 	static Dance dance = new Dance();
-	static Thread dance_run = new Thread(dance);
 
 
 	public static void main(String[] args) {
-		dance_run.start();
-		music_run.start();
-		disco_run.start();
-		
+		dance.start();
+		music.start();
+		disco.start();
+
 	}
 
 }
