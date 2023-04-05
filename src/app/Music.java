a9b8d82cee783171728b5967c49dddd2ba6ee66d
marikadu <marika.duhhanina@student.hamk.fi>
2023-04-05 09:55:45 +0300

    changed the volume of the music

--- src/app/Music.java
+++ src/app/Music.java
@@ -3,43 +3,42 @@ package app;
 import java.io.File;
 import java.util.Random;
 import lejos.hardware.Sound;
-import lejos.utility.Delay;
 
 public class Music extends Thread {
-	
+
 	Random random = new Random();
 	int min = 1;
 	int max = 4;
 	int getRandomNumberInRange;
-	int output = (int) (Math.random()*max + min);
-	
+	int output = (int) (Math.random() * max + min);
+
 	@Override
 	public void run() {
 		switch (output) {
 		case 1:
 			System.out.println("Rick Roll");
-			Sound.playSample(new File("celebration_music1.wav"), 30);
+			Sound.playSample(new File("celebration_music1.wav"), Sound.VOL_MAX);
 			break;
 
 
 		case 2:
 			System.out.println("Giga Chad");
-			Sound.playSample(new File("celebration_music3.wav"), 30);
+			Sound.playSample(new File("celebration_music3.wav"), Sound.VOL_MAX);
 			break;
 
 
 		case 3:
 			System.out.println("Calm Down");
-			Sound.playSample(new File("celebration_music4.wav"), 30);
+			Sound.playSample(new File("celebration_music4.wav"), Sound.VOL_MAX);
 			break;
 
 
 		case 4:
 			System.out.println("Tunak Tunak Tun");
-			Sound.playSample(new File("celebration_music5.wav"), 30);
+			Sound.playSample(new File("celebration_music5.wav"), Sound.VOL_MAX);
 			break;
 		}
-		
+
 	}
 
 }
