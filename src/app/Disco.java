3fd5f0825f6495d0f1be3019def44315c15e9c65
marikadu <marika.duhhanina@student.hamk.fi>
2023-04-05 11:50:31 +0300

    fixed music

--- src/app/Disco.java
+++ src/app/Disco.java
@@ -5,25 +5,25 @@ import lejos.utility.Delay;
 
 public class Disco extends Thread {
 
-	
+
 	@Override
 	public void run() {
 
 		int i = 0;
-		while (i < 9) {
-			Button.LEDPattern(1); 
+		while (i < 8) {
+			Button.LEDPattern(1); // 1 - green
 			Delay.msDelay(300);
-			Button.LEDPattern(0);
+			Button.LEDPattern(0); // 0 - nothing
 			Delay.msDelay(100);
-			Button.LEDPattern(2); 
+			Button.LEDPattern(2); // 2 - red
 			Delay.msDelay(300);
-			Button.LEDPattern(0);
+			Button.LEDPattern(0); // 0 - nothing
 			Delay.msDelay(100);
-			Button.LEDPattern(3); 
+			Button.LEDPattern(3); // 3 - orange
 			Delay.msDelay(300);
-			i++;
+			Button.LEDPattern(0); // 0 - nothing
+
 		}
 
 	}
-
 }
